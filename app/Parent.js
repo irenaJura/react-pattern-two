2ac7e8cbaf09c357e8a8784bf49c8e53b17dad03
Irena Jurasek <irenaskydive@gmail.com>
2020-02-05 16:15:28 +0100

    Fix Child in Parents render

--- app/Parent.js
+++ app/Parent.js
@@ -5,18 +5,18 @@ import { Child } from './Child';
 class Parent extends React.Component {
   constructor(props) {
     super(props);
-    this.changeName = this.changeName.bind(this);
     this.state = { name: 'Irena' };
+    this.changeName = this.changeName.bind(this);
   }
-
+  
   changeName(newName) {
     this.setState({
       name: newName
     });
   }
-
+  
   render() {
-    return <Child onChange={this.changeName} />
+    return <Child name={this.state.name} onChange={this.changeName} />
   }
 }
 
