27695029c56eaf655904e2ec58258b8d947ca509
Irena Jurasek <irenaskydive@gmail.com>
2020-02-05 16:21:49 +0100

    Add handle change method to Child

--- app/Child.js
+++ app/Child.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
 export class Child extends React.Component {
+  constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+      this.props.onChange(e.target.value);
+  }
+  
   render() {
     return (
       <div>
         <h1>
           Hey my name is {this.props.name}!
         </h1>
-        <select id="great-names">
+        <select id="great-names" onChange={this.handleChange}>
           <option value="Irena">
             Irena
           </option>
